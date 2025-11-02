97ddfd1b9b1082c975c88d18dfd8960c0cadc0bc
roy1704 <roy1704@naver.com>
2025-11-03 03:34:28 +0900

    refactor(view): InputView 입력 처리 로직 수정

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.validator.InputValidator;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -13,73 +12,42 @@ public class InputView{
 
 
     public int readMoney(){
-        while (true){
-            System.out.println("로또를 구입할 금액을 입력하세요:");
-            try{
-                return getMoneyInput();
-            } catch (IllegalArgumentException e){
-                System.out.println(e.getMessage());
-            } catch (Exception e){
-                System.out.println("[ERROR] 유효하지 않은 입력입니다. 숫자를 입력하세요.");
-            }
-        }
-    }
-
-    private int getMoneyInput(){
+        System.out.println("구입금액을 입력해 주세요.");
         String inputMoney = Console.readLine();
-        int Money = Integer.parseInt(inputMoney);
-        InputValidator.validateMoney(Money);
-        return Money; //예외 전파
+        try{
+            return Integer.parseInt(inputMoney);
+        } catch(NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
     }
 
     public List<Integer> readAnswer(){
-        while(true){
-            System.out.println("당첨 금액을 입력하세요:");
-            try{
-                return getAnswerInput();
-            }catch(IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }catch (Exception e){
-                System.out.println("[ERROR] 유효하지 않은 입력입니다. 숫자와 콤마를 사용하세요");
-            }
-        }
-    }
 
-    private List<Integer> getAnswerInput(){ // 이거 answer객체 도메인 자체에서 설정하게 고치기
+        System.out.println("당첨 번호를 입력해 주세요.");
         String inputAnswer = Console.readLine();
         String[] tokens = inputAnswer.split(",");
         List<Integer> numbers = new ArrayList<>();
 
-        for (String token : tokens) {
-            token = token.trim();
-            if (!token.isEmpty()) {
-                numbers.add(Integer.parseInt(token));
+        try{
+            for (String token : tokens) {
+                numbers.add(Integer.parseInt(token.trim()));
             }
+            return numbers;
+        } catch(NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자와 쉼표(,)로만 구성되어야 합니다.");
         }
-        InputValidator.validateAnswer(numbers);
-        return numbers;
     }
+
 
     public int readBonusAnswer(){
-        while(true){
-            System.out.println("보너스 금액을 입력하세요:");
-            try{
-                return getBonusAnswerInput();
-            }catch(IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }catch (Exception e){
-                System.out.println("[ERROR] 유효하지 않은 입력입니다. 숫자를 입력하세요.");
-            }
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        String inputBonusAnswer = Console.readLine();
+        try{
+            return Integer.parseInt(inputBonusAnswer);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자여야 합니다.");
         }
     }
-
-    private int getBonusAnswerInput(){
-        String inputBonusAnswer = Console.readLine();
-        int bonusAnswer = Integer.parseInt(inputBonusAnswer);
-        InputValidator.validateBonus(bonusAnswer);
-        return bonusAnswer;
-    }
-
 
 
 }

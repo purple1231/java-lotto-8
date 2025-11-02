1035099f5675b36454603dcfe813dc2f6049319f
roy1704 <roy1704@naver.com>
2025-11-03 03:36:48 +0900

    fix(lotto): LottoResult 계산 로직 수정

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -17,9 +17,15 @@ public class LottoResult {
     private void calculateStatistics(LottoAnswer lottoAnswer, Lottos lottos){
         for(Lotto lotto: lottos.getLottos()){
             int matchCount = matchingNumbers(lotto, lottoAnswer);
-            boolean matchBonus = matchingBonus(lotto, lottoAnswer);
 
-            Rank rank = Rank.valueOf(matchCount, matchBonus);
+            if(matchCount ==Rank.SECOND.getMatchCount()){
+                boolean matchBonus = matchingBonus(lotto, lottoAnswer);
+                Rank rank = Rank.valueOf(matchCount, matchBonus);
+                rankStatus.put(rank, rankStatus.get(rank) + 1);
+                continue;
+            }
+
+            Rank rank = Rank.valueOf(matchCount, false);
             rankStatus.put(rank, rankStatus.get(rank)+1);
         }
     }
@@ -34,5 +40,29 @@ public class LottoResult {
     private boolean matchingBonus(Lotto lotto, LottoAnswer lottoAnswer){
         return lotto.getNumbers().contains(lottoAnswer.getBonus());
     }
+
+    public Map<Rank, Integer> getRankStatus() {
+        return rankStatus;
+    }
+
+    //총 당첨금
+    public int getTotalPrizeMoney(){
+        int totalPrize = 0;
+        // key
+        for(Rank rank : rankStatus.keySet()){
+            totalPrize += rank.getPrizeMoney() * rankStatus.get(rank); // value
+        }
+        return totalPrize;
+    }
+
+    //수익률
+    public double calculateRate (int purchaseMoney){
+        if(purchaseMoney == 0){
+            return 0.0;
+        }
+        long totalPrize = getTotalPrizeMoney();
+        return (double) totalPrize / purchaseMoney * 100.0;
+    }
+
 
 }

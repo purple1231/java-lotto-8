e4cdcf8dc8afcee26079d3e261cf802fc69167b0
roy1704 <roy1704@naver.com>
2025-11-03 03:36:27 +0900

    feat(lotto): LottoCount 검증(validate) 로직 추가

--- src/main/java/lotto/domain/LottoCount.java
+++ src/main/java/lotto/domain/LottoCount.java
@@ -7,7 +7,7 @@ public class LottoCount {
     private static final int LOTTO_PRICE = 1000;
 
     public LottoCount(int money) {
-        InputValidator.validateMoney(money);
+        validateMoney(money);
         this.money = money;
     }
 
@@ -19,5 +19,14 @@ public class LottoCount {
 
     public int getMoney(){
         return money;
+    }
+
+    private void validateMoney(int money){
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
+        }
+        if (money <= 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 0보다 커야 합니다.");
+        }
     }
 }

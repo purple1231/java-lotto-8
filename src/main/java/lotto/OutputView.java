f8b9f8e9f74e7cd5e66997dfbe1ea503c90b032d
roy1704 <roy1704@naver.com>
2025-11-03 05:23:38 +0900

    refactor(InputView): InputView 오류 개선

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -7,18 +7,17 @@ import java.util.Map;
 public class OutputView {
 
     public void printLottosCount(LottoCount lottoCount){
-        System.out.printf("\n%d개를 구매했습니다.\n", lottoCount.getAmount());
+        System.out.printf("%d개를 구매했습니다.\n", lottoCount.getAmount());
     }
 
     public void printLottos(Lottos lottos){
         for(Lotto lotto : lottos.getLottos()){
             System.out.println(lotto.getNumbers());
         }
-        System.out.println();
     }
 
     public void printLottoResult(LottoResult lottoResult, LottoCount lottoCount){
-        System.out.println("\n당첨 통계");
+        System.out.println("당첨 통계");
         System.out.println("---");
 
         Map<Rank, Integer> rankStatus = lottoResult.getRankStatus();
@@ -30,6 +29,6 @@ public class OutputView {
         System.out.printf("%s - %d개\n", Rank.FIRST.getDisplayName(), rankStatus.get(Rank.FIRST));
 
         double rate = lottoResult.calculateRate(lottoCount.getMoney());
-        System.out.printf("총 수익률은 %.1f%%입니다.", rate);
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", rate);
     }
 }
